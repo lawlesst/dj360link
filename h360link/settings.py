5456ffb317a90b0170506fddd1dce7717be32de5
Ted Lawless <lawlesst@gmail.com>
2012-09-09 18:23:28 -0700

    Update to project path for static.

--- h360link/settings.py
+++ h360link/settings.py
@@ -160,7 +160,7 @@ INSTALLED_APPS += (
 
 #Static files
 import os
-PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT= os.path.join(PROJECT_PATH,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
