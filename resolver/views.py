d8a31c2deee59a4e9a0be6a7356a688d2e07181b
Ted Lawless <lawlesst@gmail.com>
2013-02-10 02:39:55 +0100

    Removed comments.

--- resolver/views.py
+++ resolver/views.py
@@ -22,27 +22,6 @@ from app_settings import SERSOL_KEY
 
 from models import Resource
 
-"""
-Examples
-
-article
-http://localhost:8000/?sid=google&auinit=S&aulast=Hammond&atitle=Agent+Orange:+Health+and+Environmental+Issues+in+Vietnam,+Cambodia,+and+Laos&id=doi:10.1002/9781118184141.ch15
-
-inbook
-http://localhost:8000/?genre=bookitem&isbn=9781405156967&title=Autism%3a+An+integrated+view+from+neurocognitive%2c+clinical%2c+and+intervention+research.&volume=&issue=&date=20080101&atitle=Teaching+adults+with+autism+spectrum+conditions+to+recognize+emotions%3a+Systematic+training+for+empathizing+difficulties.&spage=236&pages=236-259&sid=EBSCO:PsycINFO&aulast=Golan%2c+Ofer
-
-journal
-http://localhost:8000/?sid=FirstSearch%3AWorldCat&genre=journal&issn=2151-0814&title=Charter+school+law+deskbook.&id=doi%3A&pid=%3Caccession+number%3E436869608%3C%2Faccession+number%3E%3Cfssessid%3E0%3C%2Ffssessid%3E&url_ver=Z39.88-2004&rfr_id=info%3Asid%2Ffirstsearch.oclc.org%3AWorldCat&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&req_dat=%3Csessionid%3E0%3C%2Fsessionid%3E&rfe_dat=%3Caccessionnumber%3E436869608%3C%2Faccessionnumber%3E&rft_id=info%3Aoclcnum%2F436869608&rft_id=urn%3AISSN%3A2151-0814&rft.jtitle=Charter+school+law+deskbook.&rft.issn=2151-0814&rft.place=Charlottesville++VA&rft.pub=LexisNexis&rft.genre=journal&checksum=25d304e7598efe59760d18e8854b5090&title=Brown%20University&linktype=openurl&detail=RBN
-http://localhost:8000/?genre=journal&issn=0036-8075&date=1995
-
-crossref content negotiation
-http://crosstech.crossref.org/2011/11/turning_dois_into_formatted_ci.html
-
-failing resolution
-http://localhost:8000/?doi=10.1177/1527002507808309
-
-"""
-
 class ResolveView(BaseResolverView):
     template_name = 'resolver/resolve.html'
     default_json = False
